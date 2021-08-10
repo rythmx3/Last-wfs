a68a7580292067b0a2dc6aff5574d26ca91c0a06
rythmx3 <86975638+rythmx3@users.noreply.github.com>
2021-08-10 19:17:20 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
-branch=main
-git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp ultroid/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
+#lwdaa
+git clone https://github.com/rythmx3/tgvc-low-py /root/tgvc-userbot
+cp ultroid/.env /root/tgvc-userbot/.env
+cd /root/tgvc-userbot
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
